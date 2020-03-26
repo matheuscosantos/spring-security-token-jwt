b92d2881a56793c308b2aab8939510b4e9504cce
Matheus <oliveiramatheus77@gmail.com>
2020-03-26 13:29:45 -0300

    Adicionadas funções para autenticação

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$cntDoVI0NIvkHu1kM1yy6.yRR66krM2ZdlcbfIvZGqFg70f9eX6AK');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
